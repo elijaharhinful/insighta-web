3001493bd38657bb1c9cccecfeb16b7957ed5007
Elijah Arhinful <elijaharhinful8@gmail.com>
2026-04-30 01:42:57 +0000

    fix(workdlow): fix undefined url during ci run

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
 const nextConfig: NextConfig = {
   async rewrites() {
+    if (!apiUrl) {
+      console.warn("Warning: NEXT_PUBLIC_API_URL is not defined.");
+    }
+
     return [
       {
         source: "/api/proxy/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `${apiUrl || "http://localhost:3000"}/:path*`,
       },
     ];
   },
